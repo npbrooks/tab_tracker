d7b96fe9600ba5dd06587b93e2078491cf1f5282
Nathan <nbrooks.6375@gmail.com>
2019-11-17 17:16:09 -0500

    setup of express js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,18 @@
 console.log('hello')
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')//allows you to process JSON data very easily
 const cors = require('cors')
-const morgan = require('morgan')
+const morgan = require('morgan')//log generator
 
-const app = express()
-app.use(morgan('combine'))
-app.use(bodyParser.json())
+const app = express()//builds an express server. The basic web application
+app.use(morgan('combined'))//morgan prints out logs
+app.use(bodyParser.json())//parse JSON requests that are sent to it
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.get('/status', (req, res) => {//http get request
     res.send({
         message: 'hello world!'
     })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8081)//
